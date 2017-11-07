4fe05b438b19569b78c45ccf85970012915696fa
abdomokhtar <abdel@incredible-qr.com>
2017-11-07 13:30:43 +0800

    feat: helper sort descending by default.

--- frontend/src/utils/Helpers.js
+++ frontend/src/utils/Helpers.js
@@ -2,9 +2,9 @@ export const sort = (list = [], prop) => {
   if (prop) {
     return Array.prototype.sort.call(list, (a, b) => {
       if (a[prop] < b[prop]) {
-        return -1
-      } else if (a[prop] > b[prop]) {
         return 1
+      } else if (a[prop] > b[prop]) {
+        return -1
       }
       return 0
     })
